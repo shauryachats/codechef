1dcc1587ba263de172558eb1ba4400678e453ce8
Shaurya <shauryachats@gmail.com>
2016-02-01 16:31:38 +0530

    Test file updated

--- test.py
+++ test.py
@@ -1,20 +1,28 @@
 import codechef.contest
 import codechef.user
 import codechef.problem
+import codechef.utils
 import json
 
 #Testing user.getUserData()
-a = codechef.user.getUserData('shauryachats', timeOutTime = 86400)
+a = codechef.user.getUserData('shauryachats', timeOutTime = 0)
 print json.dumps(a, indent = 4)
 
 #Testing contest.getContestData()
-b = codechef.contest.getContestData('LTIME20', timeOutTime = 86400)
+b = codechef.contest.getContestData('LTIME20', timeOutTime = 0)
 print json.dumps(b, indent = 4)
 
 #Testing contest.getContestList()
-c = codechef.contest.getContestList(timeOutTime = 86400)
+c = codechef.contest.getContestList(timeOutTime = 0)
 print json.dumps(c, indent = 4)
 
 #Testing problem.getProblemData()
-d = codechef.problem.getProblemData('TEST', timeOutTime = 86400)
+d = codechef.problem.getProblemData('TEST', timeOutTime = 0)
 print json.dumps(d, indent = 4)
+
+#Testing utils.dumpData()
+codechef.utils.dumpData(d, 'test', compressed=True)
+
+#Testing utils.getData()
+e = codechef.utils.getData('test.cjson')
+print json.dumps(e, indent = 4)
